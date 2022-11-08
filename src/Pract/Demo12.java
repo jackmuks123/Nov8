3dca3ed011f64b011ffe7eba64d1efc197c6eb99
muksc <mukschavan@gmail.com>
2022-11-08 15:07:13 +0530

    First Commit

---
+++ src/Pract/Demo12.java
@@ -0,0 +1,18 @@
+package Pract;
+
+public class Demo12 {
+
+	public static void main(String[] args) {
+		int num=2;
+		if(num==2)
+		{
+			System.out.println("hi");
+		}
+		else
+		{
+			System.out.println("welcome");
+		}
+
+	}
+
+}
